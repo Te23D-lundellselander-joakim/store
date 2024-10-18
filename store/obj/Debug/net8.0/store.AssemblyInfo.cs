da5ff9fbaf486ac2ccec1d1e6584c6aa2a7cf497
joakim lundell selander <joakim.lundellselander@elev.ga.ntig.se>
2024-10-18 09:46:00 +0200

    m

--- store/obj/Debug/net8.0/store.AssemblyInfo.cs
+++ store/obj/Debug/net8.0/store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35d0b4431095b7e9b465cc684aa0a691900d38b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
